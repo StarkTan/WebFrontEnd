e77af0ebe924b37813aaa3f3b90f149a5a057535
Stark <386707933@qq.com>
2020-01-14 14:13:16 +0800

    添加 router 和 vuex 插件

--- iot_cloud/src/main.js
+++ iot_cloud/src/main.js
@@ -1,9 +1,13 @@
 // 入口文件来着，主要作用是初始化vue实例并使用需要的插件
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
